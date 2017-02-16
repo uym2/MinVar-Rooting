d39e467e0b2c8f9a1ef8ebb76cff5568ba0225fb
uym2 <ceciliamaitouyen@gmail.com>
2017-02-15 17:45:05 -0800

    Multitree FastRoot

--- FastRoot.py
+++ FastRoot.py
@@ -28,7 +28,8 @@ schema=args["schema"] if args["schema"] else "newick"
 if args["outfile"]:
 	outfile = args["outfile"]
 else:
-	outfile = base_name + "_" + args["method"] + "_rooted" + ext
+	outfile = None
+
 try:
 	os.remove(outfile)
 except:
@@ -39,29 +40,34 @@ try:
 except:
 	pass
 
-i = 1
+if args["infofile"]:
+	f_info = open(args["infofile"],'w')
+else: 
+	f_info = None
 
-#trees = TreeList.get(path=tree_file,schema=schema)
-#fin = open(tree_file,'r')
-#for tree in trees:
-tree = Tree.get(path=tree_file,schema=schema)
-if args["method"] == "MP":
-	a_tree = MPR_Tree(ddpTree=tree)
-elif args["method"] == "MD":
-	a_tree = MDR_Tree(ddpTree=tree)
-elif args["method"] == "MV":
-	a_tree = minVAR_Tree(ddpTree=tree)
-elif args["method"] == "MP2":
-	a_tree = MPR2_Tree(ddpTree=tree)
-else:
-	a_tree = None
+with open(tree_file,'r') as f:
+	for line in f:
+		tree = Tree.get(data=line,schema=schema)	
+		if args["method"] == "MP":
+			a_tree = MPR_Tree(ddpTree=tree)
+		elif args["method"] == "MD":
+			a_tree = MDR_Tree(ddpTree=tree)
+		elif args["method"] == "MV":
+			a_tree = minVAR_Tree(ddpTree=tree)
+		elif args["method"] == "MP2":
+			a_tree = MPR2_Tree(ddpTree=tree)
+		else:
+			a_tree = None
+		if a_tree is None:
+			print "Invalid method!"
+			break
+		else:
+			head_id, tail_id, edge_length, x = a_tree.Reroot()
 
-if a_tree is None:
-	print "Invalid method!"
-else:
-	head_id, tail_id, edge_length, x = a_tree.Reroot()
+		if f_info:
+			f_info.write("Head: " + str(head_id) + "\nTail: " + str(tail_id) + "\nEdge_length: " + str(edge_length) + "\nx: " + str(x)+ "\n")
+			
+		a_tree.tree_as_newick(outfile=outfile,append=True)
 
-	if args["infofile"]:
-		with open(args["infofile"],'w') as f:
-			f.write("Head: " + str(head_id) + "\nTail: " + str(tail_id) + "\nEdge_length: " + str(edge_length) + "\nx: " + str(x) + "\n")
-	a_tree.tree_as_newick(outfile=outfile)
+if f_info:
+	f_info.close()
