d9c531d30d4a3eea2339b7c9ea7db73814b4dc84
Niema Moshiri <niemamoshiri@gmail.com>
2017-09-21 14:17:20 -0700

    Fixed typo

--- FastRoot.py
+++ FastRoot.py
@@ -14,7 +14,7 @@ METHOD2FUNC = {'MP':MPR_Tree,'MV':MVR_Tree}
 parser = argparse.ArgumentParser()
 parser.add_argument('-i','--input',required=False,type=argparse.FileType('r'),default=stdin,help="Input File (default is STDIN)")
 parser.add_argument('-m','--method',required=False,type=str,default="MV",help="Method (MP for midpoint, MV for minVAR) (default is MV)")
-parser.add_argument('-o','--outfile',required=False,type=argparse.FileType('w'),default=stdout,help="Output File (default is STDIN)")
+parser.add_argument('-o','--outfile',required=False,type=argparse.FileType('w'),default=stdout,help="Output File (default is STDOUT)")
 parser.add_argument('-s','--schema',required=False,type=str,default="newick",help="Schema of your input treefile (default is newick)")
 parser.add_argument('-f','--infofile',required=False,type=argparse.FileType('w'),default=None,help="Write info of the new root to file (mostly for research and debugging purposes) (default is None)")
 args = parser.parse_args()
