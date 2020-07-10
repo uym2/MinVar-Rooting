b6f61285d8c53ad3a1e61675807578f76fc60e13
Merve <3mervekilic@gmail.com>
2020-07-10 01:24:42 -0700

    edit

--- FastRoot.py
+++ FastRoot.py
@@ -3,21 +3,28 @@
 # usage: python MP_reroot.py <tree_file>
 
 import os
-from Tree_extend import MPR_Tree,MV00_Tree,OGR_Tree
-#from dendropy import Tree,TreeList
+from Tree_extend import MPR_Tree, MV00_Tree, OGR_Tree
+# from dendropy import Tree,TreeList
 from treeswift import *
-from sys import stdin,stdout
+
+from sys import stdin, stdout
 import argparse
 
-METHOD2FUNC = {'MP':MPR_Tree,'MV':MV00_Tree,'OG':OGR_Tree}
+METHOD2FUNC = {'MP': MPR_Tree, 'MV': MV00_Tree, 'OG': OGR_Tree}
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i','--input',required=False,type=argparse.FileType('r'),default=stdin,help="Input File (default is STDIN)")
-parser.add_argument('-m','--method',required=False,type=str,default="MV",help="Method (MP for midpoint, MV for minVAR, OG for outgroup) (default is MV)")
-parser.add_argument('-g','--outgroups',required=False,type=str,help="Listing of the outgroups; to be used with -m OG")
-parser.add_argument('-o','--outfile',required=False,type=argparse.FileType('w'),default=stdout,help="Output File (default is STDOUT)")
-parser.add_argument('-s','--schema',required=False,type=str,default="newick",help="Schema of your input treefile (default is newick)")
-parser.add_argument('-f','--infofile',required=False,type=argparse.FileType('w'),default=None,help="Report the optimization score to file")
+parser.add_argument('-i', '--input', required=False, type=argparse.FileType('r'), default=stdin,
+                    help="Input File (default is STDIN)")
+parser.add_argument('-m', '--method', required=False, type=str, default="MV",
+                    help="Method (MP for midpoint, MV for minVAR, OG for outgroup) (default is MV)")
+parser.add_argument('-g', '--outgroups', required=False, type=str,
+                    help="Listing of the outgroups; to be used with -m OG")
+parser.add_argument('-o', '--outfile', required=False, type=argparse.FileType('w'), default=stdout,
+                    help="Output File (default is STDOUT)")
+parser.add_argument('-s', '--schema', required=False, type=str, default="newick",
+                    help="Schema of your input treefile (default is newick)")
+parser.add_argument('-f', '--infofile', required=False, type=argparse.FileType('w'), default=None,
+                    help="Report the optimization score to file")
 
 args = parser.parse_args()
 
@@ -26,21 +33,20 @@ assert args.method in METHOD2FUNC, "Invalid method! Valid options: MP for midpoi
 OGs = args.outgroups.split() if args.outgroups else None
 
 for line in args.input:
-    tree = read_tree(line, schema=args.schema.lower()) ####
-    #tree = Tree.get(data=line,schema=args.schema.lower(),preserve_underscores=True)
+    tree = read_tree(line, schema=args.schema.lower())
     if args.method == 'OG':
-        a_tree = OGR_Tree(OGs,ddpTree=tree)
-    else:    
+        a_tree = OGR_Tree(OGs, ddpTree=tree)
+    else:
         a_tree = METHOD2FUNC[args.method](ddpTree=tree)
-    
+
     a_tree.Reroot()
 
     if args.infofile:
         args.infofile.write(a_tree.report_score() + "\n")
 
-    #args.outfile.write(a_tree.ddpTree.as_string("newick"))     
+    # args.outfile.write(a_tree.ddpTree.as_string("newick"))
 
     a_tree.tree_as_newick(outstream=args.outfile)
 
-#if args.infofile:
-    #args.infofile.close()
+# if args.infofile:
+# args.infofile.close()
