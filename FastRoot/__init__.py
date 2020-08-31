287fc86d524ab5a7d440a48be523f97c27ecbfa7
Merve <3mervekilic@gmail.com>
2020-08-30 18:47:25 -0700

    organized files

---
+++ FastRoot/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+#############################################################################
+##  this file is part of the MinVar-Rooting package
+##  see LICENSE for terms and conditions of usage.
+#############################################################################
+
+PROGRAM_NAME = "FastRoot"
+PROGRAM_AUTHOR = ["Uyen Mai","Erfan Sayyari","Siavash Mirarab","Merve Kilic"]
+PROGRAM_LICENSE = "MIT License"
+PROGRAM_VERSION = "1"
+PROGRAM_YEAR = "2020"
+PROGRAM_INSTITUTE = "Department of Computer Science and Engineering, University of California at San Diego"
