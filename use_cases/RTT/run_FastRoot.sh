764b0d6da295075c00b3b7d612dead52aa9564d3
merve-kilic <3mervekilic@gmail.com>
2020-09-25 04:40:13 -0700

    edit use_cases

--- use_cases/RTT/run_FastRoot.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-FastRoot.py -m RTT -i input.trees -t sampling_times.txt -o outgroup.trees
---
+++ use_cases/RTT/run_RTT.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+FastRoot.py -m RTT -i input.trees -t sampling_times.txt -o output.trees
